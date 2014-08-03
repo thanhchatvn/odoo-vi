8c5f0da4527af8d8bc7878c56d0e9fbf066c43ac
Thanh Nguyen <BruceNguyen@Thanhs-MacBook-Pro.local>
2014-08-03 13:51:26 +0700

    export all vi.po

---
+++ translate.py
@@ -0,0 +1,41 @@
+import subprocess
+import os
+
+path = os.getcwd()
+arg = '*vi.po'
+
+list_path = subprocess.check_output(['find', path, '-name', arg], shell=False)
+
+sum_path = []
+print '-' * 100
+list_all = list_path.split('\n')
+print path
+
+subprocess.check_output(['mkdir', path + '/translate'])
+trans_path = path + '/translate'
+ 
+for path in list_all:
+    try:
+        print '----> path --> %s' % path
+        path_ls = path.split('/')[6]
+        print 'open --> %s' % path
+        ifile = open(path, 'rb')
+ 
+        path_out = trans_path + '/' + path_ls + '.po'
+ 
+        print 'outfile --> %s' % path_out
+        ofile = open(path_out, 'wb')
+        ofile.write(ifile.read())
+        ofile.close()
+        ifile.close()
+        #
+        print 'finish --> %s' % trans_path
+    except Exception, e:
+        print path
+        print e
+        continue
+
+
+
+
+
